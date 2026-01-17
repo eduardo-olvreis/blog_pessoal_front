eead7e7e6ab6d334b0269c185420ca2d83f45d98
Eduardo <eduardo.olvreis@gmail.com>
2026-01-16 23:16:04 -0300

    feat: authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
@@ -8,17 +9,19 @@ import Login from "./pages/login/Login";
 function App(){
   return(
     <>
-      <BrowserRouter>
-        <Navbar/>
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/cadastro" element={<Cadastro/>} />
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar/>
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login/>} />
+              <Route path="/home" element={<Home/>} />
+              <Route path="/cadastro" element={<Cadastro/>} />
+            </Routes>
+          </div>
+          <Footer/>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
