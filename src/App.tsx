c97782eba0194f94304af1789afe2056039a35ac
Eduardo <eduardo.olvreis@gmail.com>
2026-01-19 22:49:10 -0300

    feat: deletar tema

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from "./components/footer/Footer"
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
 import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
 
 function App(){
   return(
@@ -22,6 +23,7 @@ function App(){
               <Route path="/temas" element={<ListaTemas/>} />
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer/>
