2bf3d54483c0f40867d73f9097262a299f0fdba7
Eduardo <eduardo.olvreis@gmail.com>
2026-01-23 22:15:23 -0300

    feat: atualizações no footer e navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,12 @@
-import { useContext } from "react"
+import { useContext, type ReactNode } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar(){
 
   const navigate = useNavigate()
-  const { handleLogout } = useContext(AuthContext)
+
+  const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout(){
     handleLogout()
@@ -13,8 +14,10 @@ function Navbar(){
     navigate("/")
   }
 
-  return(
-    <>
+  let component: ReactNode
+
+  if(usuario.token !== ""){
+    component = (
       <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
         <div className="container flex justify-between text-lg mx-8">
           <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
@@ -27,6 +30,12 @@ function Navbar(){
           </div>
         </div>
       </div>
+    )
+  }
+
+  return(
+    <>
+      {component}
     </>
   )
 }
